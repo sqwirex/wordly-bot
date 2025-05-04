91ea970f33f834435a9f07ecc45eee7140b1ba9c
sqwirex <158517976+sqwirex@users.noreply.github.com>
2025-05-04 18:33:37 +0300

    Update bot.py

--- telegram-wordly-bot/bot.py
+++ telegram-wordly-bot/bot.py
@@ -308,7 +308,7 @@ def main():
     job_queue.run_repeating(
         send_activity_periodic,
         interval=3 * 60 * 60,  # 3 часа в секундах
-        first=0      # первый запуск сразу
+        first=60      # первый запуск сразу
     )
 	
     conv = ConversationHandler(
